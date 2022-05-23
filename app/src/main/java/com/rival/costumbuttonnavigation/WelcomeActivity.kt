fe2e47ec315bdf6877b2dd99e900c3e14ead654a
emRival <em.rival282@gmail.com>
2022-05-23 07:50:16 +0700

    login with api

--- app/src/main/java/com/rival/costumbuttonnavigation/WelcomeActivity.kt
+++ app/src/main/java/com/rival/costumbuttonnavigation/WelcomeActivity.kt
@@ -4,9 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.rival.costumbuttonnavigation.api.ApiConfig
 import com.rival.costumbuttonnavigation.fragment.ProfileFragment
 import com.rival.costumbuttonnavigation.helper.SharedPreference
+import com.rival.costumbuttonnavigation.model.ResponseUser
 import kotlinx.android.synthetic.main.activity_welcome.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -19,12 +24,47 @@ class WelcomeActivity : AppCompatActivity() {
         sPH = SharedPreference(this)
 
         btn_login.setOnClickListener {
-            sPH.setStatusLogin(true)
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
+            val email = edt_email.text.toString()
+            val password = edt_pass.text.toString()
 
-            Toast.makeText(this, "Anda Berhasil Login", Toast.LENGTH_SHORT).show()
-      finish()
+            if(email.isEmpty() || password.isEmpty()) {
+                edt_email.error = "Isi dulu !"
+                edt_pass.error = "Isi dulu !"
+                return@setOnClickListener
+            }
+
+            ApiConfig.instanceRetrofit.login(email, password).enqueue(object: Callback<ResponseUser> {
+                override fun onResponse(
+                    call: Call<ResponseUser>,
+                    response: Response<ResponseUser>
+                ) {
+                    val respon = response.body()
+
+                    if (respon != null) {
+                        if (respon.status == 0){
+                            Toast.makeText(this@WelcomeActivity, respon.message, Toast.LENGTH_SHORT).show()
+                        } else {
+                            sPH.setStatusLogin(true)
+                            val i = Intent(this@WelcomeActivity, MainActivity::class.java)
+                            Toast.makeText(
+                                this@WelcomeActivity,
+                                respon.message,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            startActivity(i)
+                            finish()
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseUser>, t: Throwable) {
+                    Toast.makeText(this@WelcomeActivity, t.localizedMessage, Toast.LENGTH_SHORT).show()
+                }
+            } )
+
+
+
+
         }
     }
 }
